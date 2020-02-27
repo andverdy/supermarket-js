348df00eb69794cc4dd3e8c71ce105a3a7c43ebf
Andrea Verdiglione <andverdy@yahoo.it>
2020-02-27 09:43:12 +0100

    creazione modifica articolo

--- js/articles.js
+++ js/articles.js
@@ -1,3 +1,4 @@
+var rowcode = "";
 function getArticles() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = callArticles;
@@ -36,58 +37,52 @@ function callArticles() {
         "<td>" +
         art.famAssDesc +
         "<td>" +
-        "<button id=update> Modifica Articolo</button>" +
+        '<input value="Modifica Articolo" type="button" onClick="getArticleByCode(\'' +
+        art.codArt +
+        "')\"/>" +
         "</td>" +
         "<td>" +
-        "<button id=insToCart> Aggiungi al carrello</button>" +
+        "<button type=button onclick=>" +
+        "Aggiungi al Carrello" +
+        "</button>" +
         "</td>" +
         "<tr>";
     }
-
+    //'<input type="button" onClick="gotoNode(\'' + result.name + '\')" />'
     document.getElementById("articleId").innerHTML = htmlArticles;
-    document.getElementById("insToCart").onclick = addToCart;
-    document.getElementById("update").onclick = update;
-    document.getElementById("addArticle").onclick = addArticle;
-    document.getElementById("addToCart").onclick = addToCart;
   }
-}
-
-function addToCart() {
-  alert("Articolo aggiungo al carrello!");
-}
-
-function update() {
-  alert("Articolo Modificato!");
 }
 
 function addArticle() {
-  window.open("file:///C:/Users/music/OneDrive/Desktop/supermarket/save.html");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = callAddArticle;
-  console.log("richiesta");
-  xmlhttp.open("PUT", "http://localhost:8080/rest/api/article/save", true);
-  xmlhttp.send();
-}
+  const xhr = new XMLHttpRequest();
 
-function callAddArticle() {
-  console.log("risposta");
-  if (this.readyState == 4 && this.status == 200) {
-    var responseObject = JSON.parse(this.responseText);
-    var article = responseObject;
-  }
-}
+  xhr.open("PUT", "http://localhost:8080/rest/api/article/save", true);
 
-function buildArticle() {
-  var article = {
-    codArt: document.getElementById("code").value,
-    descrizione: document.getElementById("descr").value,
-    pezCart: document.getElementById("pzcart").value,
-    iva: document.getElementById("iva").value,
-    famAss: document.getElementById("fam").value
+  var data = {};
+  data.codArt = document.getElementById("code").value;
+  data.descrizione = document.getElementById("descr").value;
+  data.pzCart = document.getElementById("pzcart").value;
+  data.ivaDesc = document.getElementById("iva").value;
+  data.famAssDesc = document.getElementById("fam").value;
+  var ivaId = document.getElementById("iva");
+  data.idIva = ivaId.options[ivaId.selectedIndex].value;
+  var famId = document.getElementById("fam");
+  data.idFamAss = famId.options[famId.selectedIndex].value;
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.send(JSON.stringify(data));
+
+  xhr.onload = () => {
+    console.log(xhr.responseText);
   };
-  for (var x in article) {
-    console.log("il valore oggetto article " + x + " e' " + article[x]);
-  }
+  alert("Articolo aggiungo al carrello!");
+  window.location = "file:///C:/Users/music/OneDrive/Desktop/supermarket/.html";
+}
+
+function showForm() {
+  window.location =
+    "file:///C:/Users/music/OneDrive/Desktop/supermarket/save.html";
 }
 
 function getIva() {
@@ -99,13 +94,88 @@ function getIva() {
       var iva = responseObject;
       var ivaHtml = "";
       for (var i of iva) {
-        ivaHtml += " " + i.idIva + " " + i.descrizione + " ";
+        ivaHtml +=
+          "<option value=" + i.idIva + ">" + i.descrizione + "</option>";
       }
-      console.log(ivaHtml);
     }
-    document.getElementById("demo").innerHTML = ivaHtml;
+    document.getElementById("iva").innerHTML = ivaHtml;
   };
 
   xmlhttp.open("GET", "http://localhost:8080/rest/api/iva/list", true);
   xmlhttp.send();
+}
+
+function getFamAssort() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function callFam() {
+    if (this.readyState == 4 && this.status == 200) {
+      var responseObject = JSON.parse(this.responseText);
+
+      var famAss = responseObject;
+      var famHtml = "";
+      for (var i of famAss) {
+        famHtml += "<option value=" + i.id + ">" + i.descrizione + "</option>";
+      }
+    }
+    document.getElementById("fam").innerHTML = famHtml;
+  };
+
+  xmlhttp.open("GET", "http://localhost:8080/rest/api/famassort/list", true);
+  xmlhttp.send();
+}
+
+// http://localhost:8080/rest/api/article/by-code?getCod=
+
+function getArticleByCode(code) {
+  console.log("invio la richiesta");
+  // prendi il codice dalla riga corrispondente
+
+  var url = "http://localhost:8080/rest/api/article/by-code?getCod=";
+  url += code;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = callArticleByCode;
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function callArticleByCode() {
+  console.log("in attesa di risposta");
+
+  if (this.readyState == 4 && this.status == 200) {
+    var responseObject = JSON.parse(this.responseText);
+    var codArt = responseObject.codArt;
+    var descrizione = responseObject.descrizione;
+    var pzCart = responseObject.pzCart;
+    var ivaDesc = responseObject.ivaDesc;
+    var famAssDesc = responseObject.famAssDesc;
+    var idIva = responseObject.idIva;
+    var idFamAss = responseObject.idFamAss;
+
+    console.log(
+      "stampa " +
+        codArt +
+        " " +
+        descrizione +
+        " " +
+        pzCart +
+        " " +
+        ivaDesc +
+        " " +
+        famAssDesc +
+        " " +
+        idIva +
+        " " +
+        idFamAss
+    );
+
+    window.location =
+      "file:///C:/Users/music/OneDrive/Desktop/supermarket/save.html";
+
+    // document.getElementById("code").innerHTML = ivaDesc;
+    // document.getElementById("code").innerHTML = famAssDesc;
+  }
+  document.getElementById("code").innerHTML = codArt;
+  document.getElementById("descr").innerHTML = descrizione;
+  document.getElementById("pzcart").innerHTML = pzCart;
 }
